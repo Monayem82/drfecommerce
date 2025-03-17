201a8acdd8913b94534a05b79c2a2f0fa9f889e7
Monayem <mamonayem6@gmail.com>
2025-03-18 01:20:13 +0600

    drf-spectacular

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     #External Apps
     'rest_framework',
     'mptt',
+    'drf_spectacular',
 
     #internal Apps
     'apps.product',
@@ -90,3 +91,16 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
